549626411a4d1a959a0b310e0b53b7be2968d309
Antonin-mmicro <antonin@mmicro.fr>
2026-02-12 15:02:08 +0100

    NEW COMMIT

--- Download BleachBit.ps1
+++ Download BleachBit.ps1
@@ -3,7 +3,6 @@ $url = "https://download.bleachbit.org/BleachBit-5.0.2-setup.exe"
 $path = "$env:TEMP\BleachBitSetup.exe"
 $finalpath = "C:\Program Files (x86)\BleachBit\bleachbit.exe"
 
-#Verification des drpots d'administrateur
 if (-not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
     Write-Host "Ce script doit être exécuté en tant qu'administrateur." 
     Write-Host "Script terminé" 
@@ -44,11 +43,20 @@ if (Test-Path $path) {
     Write-Host "Setup non détecté, lancement du download..."
     Invoke-WebRequest -Uri $url -OutFile $path
     Write-Host "Setup téléchargé, lancement du setup..."
-    Set-Location $env:TEMP
+    Push-Location $env:TEMP
     .\BleachBitSetup.exe /S /allusers /NoDesktopShortCut
     Start-Sleep -Seconds 30
     if (Test-Path $finalpath) {
         Write-Host "Application installé"
+        if (Test-Path $path) {
+            Write-Host "Suppression du setup..."
+            Remove-Item $path -Force
+            if (-not (Test-Path $path)) {
+                Write-Host "Setup supprimé"
+            } else { 
+                Write-Host "Impossible de supprimer le setup"
+            }
+        }
         Pop-Location
         Write-Host "Script terminé"  
     }
