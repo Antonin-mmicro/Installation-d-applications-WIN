f73a30a67ae1339a709cb4490bd85035ab1a8801
Antonin-mmicro <antonin@mmicro.fr>
2026-02-17 09:58:16 +0100

    test

--- test.ps1
+++ test.ps1
@@ -1,4 +1,31 @@
-$windowsInstaller = New-Object -ComObject WindowsInstaller.Installer
-$windowsInstaller.GetType().InvokeMember("OpenDatabase","InvokeMethod",$null,$windowsInstaller,@("C:\Users\LabDattoWin11\Downloads\DesktopEditors_x64.msi",0)) `
-  .OpenView("SELECT Value FROM Property WHERE Property='ProductVersion'") `
-  .Execute() | ForEach-Object { $_.Fetch().StringData(1) }
+$msiPath = "C:\Users\LabDattoWin11\Downloads\DesktopEditors_x64.msi"
+
+# Crée l’objet Windows Installer
+$installer = New-Object -ComObject WindowsInstaller.Installer
+
+# Ouvre la base MSI en lecture seule (0)
+$database = $installer.GetType().InvokeMember(
+    "OpenDatabase",
+    "InvokeMethod",
+    $null,
+    $installer,
+    @($msiPath, 0)
+)
+
+# Prépare la requête SQL pour récupérer ProductVersion
+$view = $database.GetType().InvokeMember(
+    "OpenView",
+    "InvokeMethod",
+    $null,
+    $database,
+    @("SELECT Value FROM Property WHERE Property='ProductVersion'")
+)
+
+# Exécute la requête
+$view.GetType().InvokeMember("Execute", "InvokeMethod", $null, $view, $null)
+
+# Récupère le résultat
+$record = $view.GetType().InvokeMember("Fetch", "InvokeMethod", $null, $view, $null)
+$version = $record.GetType().InvokeMember("StringData", "GetProperty", $null, $record, 1)
+
+$version
