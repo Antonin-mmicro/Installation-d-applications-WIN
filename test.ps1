ae2d126e34d6e2023eb3a7b132e44af213649024
Antonin-mmicro <antonin@mmicro.fr>
2026-02-17 14:51:10 +0100

    test

--- test.ps1
+++ test.ps1
@@ -1,13 +1,3 @@
-$installed = Get-ItemProperty HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\* ,
-                               HKLM:\Software\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\* `
-                               -ErrorAction SilentlyContinue |
-Where-Object { $_.DisplayName -eq "Dell SupportAssist" }
+$biosManufacturer = (Get-CimInstance -ClassName Win32_BIOS).Manufacturer
 
-if ($installed) {
-    Write-Output "Dell SupportAssist est installé."
-    exit 0
-}
-else {
-    Write-Output "Dell SupportAssist n'est pas installé."
-    exit 1
-}
+Write-Output $biosManufacturer
