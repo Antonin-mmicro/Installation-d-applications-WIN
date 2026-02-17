4ff9e066c7f7877227a14ed60d48ab34614a533f
Antonin-mmicro <antonin@mmicro.fr>
2026-02-17 09:57:21 +0100

    test

--- test.ps1
+++ test.ps1
@@ -1,2 +1,4 @@
-$msi = "C:\Users\LabDattoWin11\Downloads\DesktopEditors_x64.msi"
-(Get-WmiObject -Class Win32_Product -Filter "Name='Desktop Editors'" | Select-Object -ExpandProperty Version)
+$windowsInstaller = New-Object -ComObject WindowsInstaller.Installer
+$windowsInstaller.GetType().InvokeMember("OpenDatabase","InvokeMethod",$null,$windowsInstaller,@("C:\Users\LabDattoWin11\Downloads\DesktopEditors_x64.msi",0)) `
+  .OpenView("SELECT Value FROM Property WHERE Property='ProductVersion'") `
+  .Execute() | ForEach-Object { $_.Fetch().StringData(1) }
