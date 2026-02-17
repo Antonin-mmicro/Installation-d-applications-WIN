c8b2aadea9ed47e463a934f1b4e13e7a673bda47
Antonin-mmicro <antonin@mmicro.fr>
2026-02-17 14:56:37 +0100

    test

--- test.ps1
+++ test.ps1
@@ -1,3 +1,7 @@
 $biosManufacturer = (Get-CimInstance -ClassName Win32_BIOS).Manufacturer
 
-Write-Output $biosManufacturer
+if ($biosManufacturer -like "*Dell Inc*") {
+    Write-Host "Le fabricant du BIOS est Dell Inc. Exécution du script d'installation de SupportAssist."
+} else {
+    Write-Warning "Le fabricant du BIOS n'est pas Dell Inc. Le script d'installation de SupportAssist ne sera pas exécuté."
+}
