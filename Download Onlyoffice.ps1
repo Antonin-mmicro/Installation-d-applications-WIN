a4d6d68e2062fbc2ded9bbf4a0bab1e38d1dd7c5
Antonin-mmicro <antonin@mmicro.fr>
2026-02-17 10:14:51 +0100

    Final Version

--- Download Onlyoffice.ps1
+++ Download Onlyoffice.ps1
@@ -49,26 +49,113 @@ if (Test-Path -Path "C:\Program Files\ONLYOFFICE\DesktopEditors") {
 
 if (Test-Path -Path (Join-Path $outputDir $assetName)) {
     Write-Output "Le fichier $assetName existe déjà dans $outputDir."
-    Write-Host "Installation de $assetName ..."
-    Start-Process -FilePath "msiexec.exe" -ArgumentList "/i `"$outputDir\$assetName`" /qn" -Wait
-    Start-Sleep -Seconds 5
-    if (Test-Path -Path "C:\Program Files\ONLYOFFICE\DesktopEditors") {
-        Write-Output "Installation de $assetName terminée avec succès."
-        Write-Host "Suppression du fichier d'installation $assetName ..."
-        Remove-Item -Path (Join-Path $outputDir $assetName) -Force
-        Start-Sleep -Seconds 2
-        if (-not (Test-Path -Path (Join-Path $outputDir $assetName))) {
-            Write-Output "Fichier d'installation $assetName supprimé avec succès."
-            exit 0
+    $msiPath = "$env:USERPROFILE\Downloads\DesktopEditors_x64.msi"
+    $installer = New-Object -ComObject WindowsInstaller.Installer
+    $database = $installer.GetType().InvokeMember(
+        "OpenDatabase",
+        "InvokeMethod",
+        $null,
+        $installer,
+        @($msiPath, 0)
+    )
+    $view = $database.GetType().InvokeMember(
+        "OpenView",
+        "InvokeMethod",
+        $null,
+        $database,
+        @("SELECT Value FROM Property WHERE Property='ProductVersion'")
+    )
+    $view.GetType().InvokeMember("Execute", "InvokeMethod", $null, $view, $null)
+    $record = $view.GetType().InvokeMember("Fetch", "InvokeMethod", $null, $view, $null)
+    $versionsetup = $record.GetType().InvokeMember("StringData", "GetProperty", $null, $record, 1)
+
+    $versionsetup = [version]$versionsetup
+
+    if ($versionsetup.Major -eq $github.Major -and
+        $versionsetup.Minor -eq $github.Minor -and
+        $versionsetup.Build -eq $github.Build) 
+    {
+        Write-Host "Le fichier d'installation $assetName est déjà téléchargé et à jour."
+        Write-Host "Installation de $assetName ..."
+        Write-Host $versionsetup
+        Write-Host "Installation de $assetName ..."
+        Start-Process -FilePath "msiexec.exe" -ArgumentList "/i `"$outputDir\$assetName`" /qn" -Wait
+        Start-Sleep -Seconds 5
+        if (Test-Path -Path "C:\Program Files\ONLYOFFICE\DesktopEditors") {
+            Write-Output "Installation de $assetName terminée avec succès."
+            Write-Host "Suppression du fichier d'installation $assetName ..."
+            Remove-Item -Path (Join-Path $outputDir $assetName) -Force
+            Start-Sleep -Seconds 2
+            if (-not (Test-Path -Path (Join-Path $outputDir $assetName))) {
+                Write-Output "Fichier d'installation $assetName supprimé avec succès."
+                exit 0
+            } else {
+                Write-Warning "Impossible de supprimer le fichier d'installation $assetName. Veuillez le supprimer manuellement."
+                exit 1
+            }
         } else {
-            Write-Warning "Impossible de supprimer le fichier d'installation $assetName. Veuillez le supprimer manuellement."
+            Write-Error "L'installation de $assetName a échoué."
             exit 1
         }
     } else {
-        Write-Error "L'installation de $assetName a échoué."
-        exit 1
+        Write-Host "Le fichier d'installation $assetName est déjà téléchargé mais pas à jour. Version actuelle : $versionsetup, version la plus récente : $githubTag."
+        Write-Host "Mise à jour du fichier d'installation $assetName ..."
+        Remove-Item -Path (Join-Path $outputDir $assetName) -Force
+        Start-Sleep -Seconds 2
+        $releasesUrl  = "https://api.github.com/repos/$repoOwner/$repoName/releases"
+        $releasesJson = Invoke-RestMethod -Uri $releasesUrl -Headers @{
+            "User-Agent" = "PowerShell"
+        }
+
+        $selectedRelease = $releasesJson |
+            Where-Object { 
+                $_.assets | Where-Object { $_.name -eq $assetName } 
+            } |
+            Sort-Object {[datetime]$_.published_at} -Descending |
+            Select-Object -First 1
+
+        If (-not $selectedRelease) {
+            Write-Error "Impossible de trouver une release avec $assetName"
+            exit 1
+        }
+
+        $asset = $selectedRelease.assets | Where-Object { $_.name -eq $assetName }
+        $downloadUrl = $asset.browser_download_url
+
+        Write-Output "Release trouvée : $($selectedRelease.tag_name)"
+        Write-Output "Téléchargement de $assetName ..."
+
+        $outputFile = Join-Path $outputDir $assetName
+        Invoke-WebRequest -Uri $downloadUrl -OutFile $outputFile -Headers @{
+            "User-Agent" = "PowerShell"
+        }
+        Start-Sleep -Seconds 5
+        if (Test-Path -Path $outputFile) {
+            Write-Output "Téléchargement de $assetName terminé avec succès."
+            Write-Host "Installation de $assetName ..."
+            Start-Process -FilePath "msiexec.exe" -ArgumentList "/i `"$outputFile`" /qn" -Wait
+            Start-Sleep -Seconds 5
+            if (Test-Path -Path "C:\Program Files\ONLYOFFICE\DesktopEditors") {
+                Write-Output "Installation de $assetName terminée avec succès."
+                Write-Host "Suppression du fichier d'installation $assetName ..."
+                Remove-Item -Path (Join-Path $outputDir $assetName) -Force
+                Start-Sleep -Seconds 2
+                if (-not (Test-Path -Path (Join-Path $outputDir $assetName))) {
+                    Write-Output "Fichier d'installation $assetName supprimé avec succès."
+                    exit 0
+                } else {
+                    Write-Warning "Impossible de supprimer le fichier d'installation $assetName. Veuillez le supprimer manuellement."
+                    exit 1
+                }
+            } else {
+                Write-Error "L'installation de $assetName a échoué."
+                exit 1
+            }
+        } else {
+            Write-Error "Le téléchargement de $assetName a échoué."
+            exit 1
+        }
     }
-    
 } else {
     $releasesUrl  = "https://api.github.com/repos/$repoOwner/$repoName/releases"
     $releasesJson = Invoke-RestMethod -Uri $releasesUrl -Headers @{
