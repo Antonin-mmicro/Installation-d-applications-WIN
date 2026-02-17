f68b98a4fb89420566700a49b5a8bb82d12d23aa
Antonin-mmicro <antonin@mmicro.fr>
2026-02-17 09:05:37 +0100

    test

--- test.ps1
+++ test.ps1
@@ -1,1 +1,10 @@
-(Invoke-RestMethod "https://api.github.com/repos/ONLYOFFICE/DesktopEditors/releases").tag_name
+$selectedRelease = (Invoke-RestMethod "https://api.github.com/repos/ONLYOFFICE/DesktopEditors/releases").tag_name
+
+$selectedRelease = $releasesJson |
+        Where-Object { 
+            $_.assets | Where-Object { $_.name -eq $assetName } 
+        } |
+        Sort-Object {[datetime]$_.published_at} -Descending |
+        Select-Object -First 1
+
+Write-Host "$releasesJson"
